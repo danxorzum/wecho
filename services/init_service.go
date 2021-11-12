a661947c35f4f39a9e5b3e36c96b6b46b79dea0d
Miguel aldrete <migue_danxor@hotmail.com>
2021-11-11 20:10:13 -0600

    crea proyecto e inicializa

--- services/init_service.go
+++ services/init_service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"log"
+	"os/exec"
+
 	"github.com/culturadevops/jgt/jio"
 	"github.com/spf13/viper"
 )
@@ -9,4 +12,18 @@ func InitP(projectName string) {
 	jio.CreateFolder(viper.GetString("cmddir") + "/" + projectName)
 	projectPath := viper.GetString("cmddir") + "/" + projectName
 	jio.NewFileforTemplate(projectPath+"/main.go", viper.GetString("homedir")+"/templates/init/main.stub", nil)
+
+	cmd := exec.Command("go", "mod", "init", projectName)
+	cmd.Dir = viper.GetString("cmddir") + "/" + projectName
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	tidy := exec.Command("go", "mod", "tidy")
+	tidy.Dir = viper.GetString("cmddir") + "/" + projectName
+	err = tidy.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
